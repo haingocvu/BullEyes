421b87d8b107a2d51510995ec838ebcb888e82b6
Hai Vu <haingocvu@hotmail.com>
2019-04-19 10:13:36 +0700

    get slider value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Alert", message: "This is my first app", preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-        alert.addAction(alertAction)
-        present(alert, animated: true, completion: nil)
-    }
+	
+	@IBOutlet weak var slider: UISlider!
+	var currentValue = 0
+	var targetValue = 0
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		currentValue = lroundf(slider.value)
+		targetValue = Int.random(in: 1...100)
+	}
+	
+	@IBAction func showAlert() {
+		let message = "the value of slider is: \(currentValue)"
+		let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+		let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+		alert.addAction(alertAction)
+		present(alert, animated: true, completion: nil)
+	}
+	@IBAction func sliderMove(_ slider: UISlider) {
+		currentValue = lroundf(slider.value)
+	}
 }
 
